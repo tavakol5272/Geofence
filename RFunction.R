77e3b87054788496f3b1083961088db4b9336a00
tavakol5272 <148060212+tavakol5272@users.noreply.github.com>
2026-02-19 15:08:01 +0100

    new version

--- RFunction.R
+++ RFunction.R
@@ -1,89 +1,38 @@
-library('move2')
-library(sf)
-library(ggplot2)
-library(tidyverse)
+library("moveapps")
+library("move2")
+library("lubridate")
 
- data <- readRDS("./data/raw/Yahatinda_move2.rds")
-# 
-  
-rFunction = function(data, polygon = NULL) {
-  
-  polygon_boundary <- read_sf(paste0(getAppFilePath("polygon"),"polygon_boundary.shp"))
-  
-  ##transform the data into data-frame and sf files
-  coords <- sf::st_coordinates(data)
-  original_track_id_column <- mt_track_id_column(data)
+## The parameter "data" is reserved for the data object passed on from the previous app
 
-  data_df <- data |>
-    mutate(
-      location_long = coords[, 1],
-      location_lat = coords[, 2],
-      trackId = original_track_id_column
-    ) |>
-    as.data.frame()
+# to display messages to the user in the log file of the App in MoveApps
+# one can use the function from the logger.R file:
+# logger.fatal(), logger.error(), logger.warn(), logger.info(), logger.debug(), logger.trace()
 
-  #data_df <-as.data.frame(data)
-  data_sf <- st_as_sf(data_df, coords = c("location_long", "location_lat"), 
-  crs = st_crs(4326))
-  
-  trck_data <- data_sf %>% 
-    group_by(trackId) %>%
-    summarise(do_union = FALSE) %>%
-    st_cast("LINESTRING")
-  
-  bndry_crop <-st_crop(polygon_boundary, data_sf) ## crop the area boundaries wrt the dataset
-  ###check if there are no protected area
-  
-  if (nrow(bndry_crop)==0) 
-  {
-    logger.info("There is no intersection of your track(s) with any polygon in the Y2Y region. Returning input data set.")
-    final_dat <- data
-  } else
-  {
-  #bndry_crop1 <- bndry |> filter( PA_NAME %in% bndry_crop$PA_NAME)
-  cent_df <-st_centroid(bndry_crop)
- 
-  
-  ### filter which points are within the protected area polygon or not
-  dt <- as.matrix(st_within( data_sf, bndry_crop))
-  dat <-data.frame(column = col(dt)[dt], row = row(dt)[dt]) ###extract the index of points inside the polygon
-  data_df$within <-0
-  data_df$within[dat[,2]] <-1
-  data_df$area <- ifelse(data_df$within ==1, bndry_crop$NAME[dat[,1]], NA) ## Include the area name in the dataset
-  
-  data_sf$within <-0      
-  data_sf$within[dat[,2]] <-1
-  
-  ### Plot the points and check if the assignment is correct
-  pa_data <-ggplot()+
-    geom_sf(data = bndry_crop, fill= "grey")+
-    geom_sf(data = trck_data, size = 0.5)+
-    geom_sf(data = data_sf, size=0.5, aes(col= as.factor(within)), alpha = 0.7)+
-    geom_sf_text(data = cent_df, aes(label = NAME))+
-    labs(y = "Latitude", x = "Longitude", col = "Points\nInside/Outside")+
-    scale_color_discrete(labels = c("Outside", "Inside"))+
-    theme_bw()
-  
-  #pa_data
-  
-  ## Save the output in csv and jpeg 
-  ggsave(pa_data, filename = paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"PA_locations.jpeg"), 
-         height = 6, width = 9, units = "in", dpi = 300)
-  
-  ## converts the data-frame to movestack
-  mt_as_move2(
-    data_df,
-      coords = c("location_long", "location_lat"),
-      time_column = "timestamp",
-      crs = 4326,
-      track_id_column = original_track_id_column,
-      track_attributes = names(track_attributes)
-    )
-   
-##    final_dat <- move(x= as.numeric(data_df$location.long), y = as.numeric(data_df$location.lat),
-##                   time = as.POSIXct(data_df$timestamp, format = "%Y-%m-%d %H:%M:%S"),
-##                    data = data_df, proj = CRS("+proj=longlat +ellps=WGS84"), 
-##                    animal = data_df$trackId)
+# Showcase injecting app setting (parameter `year`)
+rFunction = function(data, sdk, year, ...) {
+  logger.info(paste("Welcome to the", sdk))
+  result <- if (any(lubridate::year(move2::mt_time(data)) == year)) { 
+    data[lubridate::year(move2::mt_time(data)) == year,]
+  } else {
+    NULL
   }
-  return(final_dat)
- }
+  if (!is.null(result)) {
+    # Showcase creating an app artifact. 
+    # This artifact can be downloaded by the workflow user on Moveapps.
+    artifact <- appArtifactPath("plot.png")
+    logger.info(paste("plotting to artifact:", artifact))
+    png(artifact)
+    plot(result[move2::mt_track_id_column(result)], max.plot=1)
+    dev.off()
+  } else {
+    logger.warn("nothing to plot")
+  }
+  # Showcase to access a file ('auxiliary files') that is 
+  # a) provided by the app-developer and 
+  # b) can be overridden by the workflow user.
+  fileName <- getAuxiliaryFilePath("auxiliary-file-a")
+  logger.info(readChar(fileName, file.info(fileName)$size))
+
+  # provide my result to the next app in the MoveApps workflow
+  return(result)
+}
--- sdk.R
+++ sdk.R
@@ -1,17 +1,18 @@
-library(dotenv)
 # You can control your local app development via environment variables.
 # You can define things like input-data, app-configuration etc.
 # Per default your environment is defined in `/.env`
-load_dot_env()
+dotenv::load_dot_env()
 
-# provide common stuff
-source("src/common/logger.R")
-source("src/common/runtime_configuration.R")
-clearRecentOutput()
-# This will parse a JSON file containing the concrete configuration of
-# the app run. Per default the file `/app-configuration.json` will be parsed.
-args <- configuration()
+# This loads and installs the MoveApps R SDK
+remotes::install_github("movestore/moveapps-sdk-r-package")
+moveapps::logger.init()
+moveapps::clearRecentOutput()
+
+library("moveapps")
+Sys.setenv(tz="UTC")
+# `./RFunction.R` is the home of your app code
+# It is the only file which will be bundled into the final app on MoveApps
+source("RFunction.R")
 
 # Lets simulate running your app on MoveApps
-source("src/moveapps.R")
-simulateMoveAppsRun(args)
+moveapps::runMoveAppsApp()
